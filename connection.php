41a70da298aa1f60cc7a747b832cebce4a4dcbad
khaled alabdullah <108240514+KhaledKhlefAlabdullah@users.noreply.github.com>
2022-12-30 22:55:01 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost="localhost";
-$dbuser="id19983546_root";
-$dbpss="Khaled@20012";
-$dbname="id19983546_login_sample_db";
+$dbuser="root";
+$dbpss="";
+$dbname="login_sample_db";
 if(!$con=mysqli_connect($dbhost,$dbuser,$dbpss,$dbname)){
     die("failed to connect");
 }
